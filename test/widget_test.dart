35dc8fa31bb7372c2e7d8b47485399469846af6f
BrendowLincoln <linconlbrendow@gmail.com>
2020-11-18 21:31:10 -0300

    Trabalhando com o widget stateless dentro da aplicação

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:seu_nivel_programador/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(SeuNivelProgramador());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
